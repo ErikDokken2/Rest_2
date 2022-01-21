bf85f9b85a9160141e5387f5d181285fdcff7a42
Erik Dokken <84480951+ErikDokken2@users.noreply.github.com>
2022-01-21 14:57:49 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AB {
+ // Prints all strings with exactly x A's and y B's in alphabetical
+ // order.
+ public static void printWrapper(int x, int y) {
+ printAll(new char[x+y], 0, x, y);
+ }
+ public static void printAll(char[] buffer, int k, int x, int y) {
+ // FILL IN CODE HERE
+ }
+ public static void main(String[] args) {
+ printWrapper(5, 3);
+ }
+}
